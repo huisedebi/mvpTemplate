cda8bfdfeb737132c80434aaffcffe06c32a0397
GitHub Action <action@github.com>
2021-06-07 02:28:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/huisedebi/mvptemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/huisedebi/mvptemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.huisedebi.mvptemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.huisedebi.mvptemplate.MyBundle
 
 class MyApplicationService {
 
